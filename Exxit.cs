0651b7e26cd871113947f8b7c3b4099e578b9701
8MVR3 <121437690+8MVR3@users.noreply.github.com>
2022-12-26 16:08:50 +0300

    Add files via upload

---
+++ Exxit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exxit : MonoBehaviour
+{
+    public GameObject GameStartUI;
+
+    public void Exitgame()
+    {
+        Application.Quit();
+    }
+}
